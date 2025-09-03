d796a887d60783fde0a6ed3174558c14f7b392f1
Vince Quiles <vjquiles@msn.com>
2025-09-03 12:58:17 -0400

    Get all projects successful, also added the user id into the return for login, wanted to compare that to the ids in project, just to be 100% sure

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,7 +6,6 @@ async function createProject(req, res) {
             ...req.body,
             user: req.user._id
         })
-        console.log(project)
         return res.status(201).json(project)
     } catch (error) {
         console.error(`Error creating project: ${error}`)
@@ -14,6 +13,17 @@ async function createProject(req, res) {
     }
 }
 
+async function getAllProjects(req, res) {
+    try {
+        const projects = await Project.find({ user: req.user._id })
+        return res.json(projects)
+    } catch (error) {
+        console.error(`Error getting all projects: ${error}`)
+        return res.status(500).json({ error: "Error getting your projects" })
+    }
+}
+
 module.exports = {
-    createProject
+    createProject,
+    getAllProjects,
 }
--- routes/api/projectRoutes.js
+++ routes/api/projectRoutes.js
@@ -6,7 +6,7 @@ const verifyUser = require('../../middleware/auth')
 router.use(verifyUser)
 
 router.post('/create-project', projectController.createProject)
-// router.get('/user-projects', projectController.getAllProjects)
+router.get('/user-projects', projectController.getAllProjects)
 // router.get('/user-projects/:id', projectController.getOneProject)
 // router.put('/update-project/:id', projectController.updateProject)
 // router.delete('/delete-project/:id', projectController.deleteProject)
