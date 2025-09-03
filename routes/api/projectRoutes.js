0e2a0cc16aabecb7d061c77f03cd34187d146b49
Vince Quiles <vjquiles@msn.com>
2025-09-03 10:55:17 -0400

    Project routes set up. Time for the controller.

---
+++ routes/api/projectRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const projectController = require('../../controllers/productController')
+const taskController = require('../../controllers/taskController')
+const verifyUser = require('../../middleware/auth')
+
+router.use(verifyUser)
+
+router.post('/create-project', projectController.createProject)
+router.get('/user-projects', projectController.getAllProjects)
+router.get('/user-projects/:id', projectController.getOneProject)
+router.put('/update-project/:id', projectController.updateProject)
+router.delete('/delete-project/:id', projectController.deleteProject)
+
+module.exports = router
